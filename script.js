93612f8f30302009f321caa83dd29c7e39554982
Clément DELCOURT <clm.delcourt@gmail.com>
2025-09-19 11:43:16 +0200

    Test

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 fetch('https://test-delta-puce-48.vercel.app/year')
 .then(response => response.json())
 .then(data => {
-    const date = data['date'];
+    const date = data['year'];
     document.querySelector('#year').innerHTML += `
     <p>FAKE_YEAR_FOR_TEST : ${date}</p>
     `;
