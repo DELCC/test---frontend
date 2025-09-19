bdb7462e683ac33495766240502601090487c01e
Clément DELCOURT <clm.delcourt@gmail.com>
2025-09-19 11:12:02 +0200

    Second Commit

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-fetch('http://localhost:3000/year')
+fetch('https://test-delta-puce-48.vercel.app/year')
 .then(response => response.json())
 .then(data => {
     document.querySelector('#year').innerHTML += `
